d459ebf7b272b1436b8e1e8adccd2b10d08d6bbe
Enxos <23334313+Enxos@users.noreply.github.com>
2024-04-30 20:14:26 +0200

    Gyoztes hirdetese es tipp ellenorzese.

--- src/Csigaverseny.java
+++ src/Csigaverseny.java
@@ -5,14 +5,30 @@ public class Csigaverseny {
     public static final double SPEED_BOOST_CHANCE = 0.2;
 
     public static void main(String[] args) {
-        // Snails
+        // Csiguszok letrehozasa
         Snail pirosSnail = new Snail("Piros");
         Snail zoldSnail = new Snail("Zold");
         Snail kekSnail = new Snail("Kek");
     
-        // Welcome message and get user bet
+        // Udvozlo uzenet es tipp kerese
         System.out.println("Udv a Csigaversenyben!");
         String bet = placeBet();
         
+        // A verseny lefuttatasa
+        for (int round = 1; round <= NUM_ROUNDS; round++) {
+            System.out.println("Round " + round);
+            runRaceRound(pirosSnail, zoldSnail, kekSnail);
+        }
+    
+        // Gyoztes meghatarozasa
+        Snail winner = determineWinner(pirosSnail, zoldSnail, kekSnail);
+        System.out.println("A gyoztes " + winner.getColor() + "!");
+    
+        // Ellenorzes, hogy a felhasznalo nyert-e
+        if (winner.getColor().equals(bet)) {
+            System.out.println("Gratulalok! Helyesen tippeltel!");
+        } else {
+            System.out.println("Sajnaljuk, nem talaltad el a nyertest. Tobb szerencset legkozelebb!");
+        }
     }
 }
